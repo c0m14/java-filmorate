748fa87769cb8fd44b256fde1c96a83628d80547
Nikita Komochkov <ascii2@yandex.ru>
2023-03-17 20:36:09 +0300

    feat: add UserController for /users

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,2 +1,112 @@
-package ru.yandex.practicum.filmorate.controller;public class UserController {
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.InvalidFilmFieldsException;
+import ru.yandex.practicum.filmorate.exceptions.InvalidUserFieldsException;
+import ru.yandex.practicum.filmorate.exceptions.UserNotExistException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    private Map<Integer, User> users = new HashMap<>();
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private int idCounter = 1;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) throws InvalidUserFieldsException {
+        log.debug("Got request to create user: {}", user);
+        checkUserFields(user, RequestType.CREATE);
+        user.setId(idCounter++);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) throws InvalidUserFieldsException, UserNotExistException {
+        log.debug("Got request to update user: {}", user);
+        checkUserFields(user, RequestType.UPDATE);
+        if (!users.keySet().contains(user.getId())) {
+            log.error("User with id {} doesn't exist", user.getId());
+            throw new UserNotExistException(
+                    String.format("User with id %d doesn't exist", user.getId())
+            );
+        }
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    private void checkUserFields(User user, RequestType requestType) throws InvalidUserFieldsException {
+        checkUserId(user.getId(), requestType);
+        checkUserEmail(user.getEmail());
+        checkUserLogin(user.getLogin());
+        checkUserName(user);
+        checkUserBirthdate(user.getBirthday());
+
+    }
+
+    private void checkUserId(int id, RequestType requestType) throws InvalidUserFieldsException {
+        if (requestType.equals(RequestType.CREATE)) {
+            if (id != 0) {
+                log.error("\"Id\" shouldn't be sent while creation");
+                throw new InvalidFilmFieldsException("\"Id\" shouldn't be sent while creation");
+            }
+        } else if (requestType.equals(RequestType.UPDATE)) {
+            if (id <= 0) {
+                log.error("\"Id\" isn't positive: {}", id);
+                throw new InvalidFilmFieldsException(
+                        String.format("\"Id\" isn't positive: %d", id));
+            }
+        }
+    }
+
+    private void checkUserEmail(String email) throws InvalidUserFieldsException {
+        if (email.isBlank() || !email.contains("@")) {
+            log.error("\"Email\" isn't correct: {}", email);
+            throw new InvalidUserFieldsException(
+                    String.format("\"Email\" isn't correct: %s", email)
+            );
+        }
+    }
+
+    private void checkUserLogin(String login) throws InvalidUserFieldsException {
+        if (login.isBlank() || login.contains(" ")) {
+            log.error("\"Login\" shouldn't be empty or contain spaces: {}", login);
+            throw new InvalidUserFieldsException(
+                    String.format("\"Login\" shouldn't be empty or contain spaces: %s", login)
+            );
+        }
+    }
+
+    private void checkUserName(User user) {
+        if (user.getName() == null) {
+            log.warn("Name is empty, login used instead");
+            user.setName(user.getLogin());
+        }
+    }
+
+    private void checkUserBirthdate(LocalDate birthday) throws InvalidUserFieldsException {
+        if (birthday.isAfter(LocalDate.now())) {
+            log.error("\"Birthdate\" is incorrect: {}", formatter.format(birthday));
+            throw new InvalidUserFieldsException(
+                    String.format("\"Birthdate\" is incorrect: %s", formatter.format(birthday))
+            );
+        }
+    }
 }
