44c1b53a18d1fa972e88684927eb4c157b8a2a4d
Nikita Komochkov <ascii2@yandex.ru>
2023-03-17 20:37:37 +0300

    feat: add enum for Controllers

--- src/main/java/ru/yandex/practicum/filmorate/controller/RequestType.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/RequestType.java
@@ -1,2 +1,6 @@
-package ru.yandex.practicum.filmorate.controller;public class RequestType {
+package ru.yandex.practicum.filmorate.controller;
+
+public enum RequestType {
+    CREATE,
+    UPDATE
 }
