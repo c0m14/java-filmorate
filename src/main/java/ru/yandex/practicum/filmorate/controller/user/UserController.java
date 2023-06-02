f7efd2c721fdbefe5e99a28d047f02901b19b27e
Nikita Komochkov <ascii2@yandex.ru>
2023-06-02 11:30:12 +0300

    fix: imports

--- src/main/java/ru/yandex/practicum/filmorate/controller/user/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/user/UserController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.InvalidUserFieldsException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.recommendations.RecommendationsService;
